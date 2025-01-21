9e8db1d58f63fcd978d60c6fdfb6c27b67f652a9
GokulaLakshmiP <gokula.lakshmip.2005@gmail.com>
2025-01-22 01:45:02 +0530

    Add files via upload

---
+++ Input and Output(try it program).py
@@ -0,0 +1,10 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Type this in the python shell window-Try it
+>>> #Write a program that 1.Asks the user for a verb 2.Prints"I can_better than you" where you replace_with the verb 3.Then prints the verb 5 times in a row seperated by spaces
+>>> question=input('Choose a verb')
+Choose a verb write
+>>> print('I can',question,'better than you!')
+I can  write better than you!
+>>> print(question*5)
+ write write write write write
